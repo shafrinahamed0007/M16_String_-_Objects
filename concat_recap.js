4c18d885ff0249cca1c51c0782a466b4e23a5143
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-01-28 12:03:01 +0600

    Reverse a string in three different ways

---
+++ concat_recap.js
@@ -0,0 +1,7 @@
+const firstName = "Shafrin";
+const lastName = "Ahamed";
+
+const fullName = firstName.concat(" " ).concat(lastName);
+console.log(fullName);
+
+console.log(lastName.includes("d"));
---
+++ slice_recap.js
@@ -0,0 +1,16 @@
+const address = "Andorkilla";
+console.log(address);
+
+console.log(address.slice(0, 5));
+
+const sentence = "I am a good and hardworking person";
+console.log(sentence);
+const sliceSentence = sentence.split(" ");
+console.log(sliceSentence);
+
+const friendsStr = "Rahim, Kahim, Dahim, Fahim";
+const friend = friendsStr.split(",");
+console.log(friend);
+
+const realFriend = [ 'Rahim', ' Kahim', ' Dahim', ' Fahim' ];
+console.log(realFriend.join(" | "));
