4e8579298d15aa231138b12c1278a34a3916085f
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-01-28 08:10:15 +0600

    String Slice, Join, Concat and Includes

---
+++ concat.js
@@ -0,0 +1,13 @@
+const first = 'Abid';
+const last = "nabid";
+
+
+const fullName = first + ' ' + last;
+// console.log(fullName);
+
+const fullName1 = first.concat(' ').concat(last);
+console.log(fullName1);
+
+
+// includes
+console.log(last.includes('d'));
---
+++ slice.js
@@ -0,0 +1,15 @@
+const address = "andorkilla"
+const part = address.slice(0,5)
+console.log(part);
+
+const sentence = "I am a good and hardworking person";
+console.log(sentence.split(' '));
+console.log(sentence.split('a'));
+
+const friendsStr = "Rahim, kahim, dahim, lahim, fahim, sahim";
+const friends = friendsStr.split(',');
+console.log(friends);
+
+const realFriend = [ 'Rahim', ' kahim', ' dahim', ' lahim', ' fahim', ' sahim' ];
+console.log(realFriend.join('|'));
+console.log(realFriend.join('-'));
