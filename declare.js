fedf3c08078f0f498578c552ae04a72fe6603515
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-01-28 15:18:03 +0600

    Loop object and ways to declare an object

---
+++ declare.js
@@ -0,0 +1,13 @@
+const pen= {
+    brand: "Econo",
+    price: 10,
+    color: "black"
+}
+
+const pencil = new Object();
+console.log(pencil);
+
+const rubber = Object.create({});
+console.log(rubber);
+
+// using new
